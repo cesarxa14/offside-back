57c872369428111484642703bb84c01c3eafae9f
Cesar Torres <cesarf9t@hotmail.com>
2021-01-25 03:49:45 -0500

    fdfdsfsdds

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('port',process.env.PORT || 3000)
 
 app.use(express.json())
 app.use('/upload', express.static(path.resolve('upload')));
+app.use(express.static(path.join(__dirname, '../offside-front')));
 app.use(require('./rutas/routes_index'));
 app.listen(app.get('port'), ()=>{
     console.log('Server on Port', app.get('port'));
